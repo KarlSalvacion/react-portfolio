f13c162ee44391c4081f839859a54949fa7e42eb
Karl Vincent <karl_vincent_salvacion@dlsl.edu.ph>
2024-09-15 19:00:42 +0800

    added contents of website

---
+++ src/introduction.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './introduction.css';
+import karlpic from "../src/images/karlreal.png";
+import jamesreidpic from "../src/images/JAMESREID.JPG";
+
+const Introduction = () => {
+  const [image, setImage] = useState(karlpic);
+  const [flip, setFlip] = useState(false); // new state to track flip state
+
+  const handleClick = () => {
+    setFlip(true);
+    setTimeout(() => {
+      setImage(image === karlpic ? jamesreidpic : karlpic);
+      setFlip(false);
+    }, 450); 
+  };
+
+  return (
+    <div id="Home" className="introduction">
+      <div className="textimage">
+        <div className="textcontent">
+          <h2 className="intro">Hello! I am</h2>
+          <h1 className="firstname">Karl Vincent</h1>
+          <h1 className="lastname">Salvacion</h1>
+          <p className="textintro">I am currently 3rd year student in De La Salle Lipa and this is my portfolio</p>
+
+          <button type="button" class="btn btn-outline-secondary custom-button" onClick={() => {
+            const projectsSection = document.getElementById('Projects');
+            const offset = 100;
+            const scrollPosition = projectsSection.offsetTop - offset;
+            window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
+          }}>CONTINUE</button>
+        </div>
+        <div className="image-container">
+          <img
+            src={image}
+            alt={image === karlpic ? 'karl pic' : 'james reid pic'}
+            className={`picme ${flip ? 'flip' : ''}`}
+            onClick={handleClick} 
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
---
+++ src/navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './navbar.css';
+import { Link } from 'react-scroll';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg custom-navbar">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="Home" smooth={true} duration={250} offset={-50}>Karl's Portfolio</Link>
+        
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="Home" smooth={true} duration={250} offset={-50}>Home</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="Projects" smooth={true} duration={250} offset={-100}>Projects</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="contactMe" smooth={true} duration={250} offset={-80}>Contact</Link>
+            </li>
+            
+            <li className="nav-item">
+              <Link className="nav-link" to="Home" smooth={true} duration={250} offset={-50}>About me</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
